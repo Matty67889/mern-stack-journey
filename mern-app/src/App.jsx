b0c497cab06ee0caf4eebee8babe5ea73f564cf7
Matthew Mentis-Cort <matty67889@gmail.com>
2025-06-15 13:36:06 -0400

    Create React Component
    - Create HelloWorld React Component
    to test making React Components

--- mern-app/src/App.jsx
+++ mern-app/src/App.jsx
@@ -1,11 +1,18 @@
-const element = (
-  <div title="Outer div">
-    <h1>Hello World!Hello</h1>
-    <h1>I want to make another h1</h1>
-    <h1>Making another change</h1>
-    <h2>Are you watching????</h2>
-    <h3>You are watching. Yay!</h3>
-  </div>
-);
+class HelloWorld extends React.Component {
+  render() {
+    return (
+      <div title="Outer div">
+        <h1>Hello World!Hello</h1>
+        <h1>I want to make another h1</h1>
+        <h1>Making another change</h1>
+        <h2>Are you watching????</h2>
+        <h3>You are watching. Yay!</h3>
+        <h1>Making a change to HelloWorld</h1>
+      </div>
+    );
+  }
+}
+
+const element = <HelloWorld />
 
 ReactDOM.render(element, document.getElementById('content'));
