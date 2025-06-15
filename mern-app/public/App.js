dd18cc3a95b83759e66d003b3dc0629d943564cd
Matthew Mentis-Cort <matty67889@gmail.com>
2025-06-15 13:20:39 -0400

    Add automation
    - Add scripts to automate watching
    for changes and compiling with Babel
    - Add HTML to test that automation is working

--- mern-app/public/App.js
+++ mern-app/public/App.js
@@ -1,4 +1,6 @@
-const element = /*#__PURE__*/React.createElement("div", {
+"use strict";
+
+var element = /*#__PURE__*/React.createElement("div", {
   title: "Outer div"
-}, /*#__PURE__*/React.createElement("h1", null, "Hello World!"));
+}, /*#__PURE__*/React.createElement("h1", null, "Hello World!Hello"), /*#__PURE__*/React.createElement("h1", null, "I want to make another h1"), /*#__PURE__*/React.createElement("h1", null, "Making another change"), /*#__PURE__*/React.createElement("h2", null, "Are you watching????"), /*#__PURE__*/React.createElement("h3", null, "You are watching. Yay!"));
 ReactDOM.render(element, document.getElementById('content'));
--- mern-app/src/App.jsx
+++ mern-app/src/App.jsx
@@ -1,6 +1,10 @@
 const element = (
   <div title="Outer div">
-    <h1>Hello World!</h1>
+    <h1>Hello World!Hello</h1>
+    <h1>I want to make another h1</h1>
+    <h1>Making another change</h1>
+    <h2>Are you watching????</h2>
+    <h3>You are watching. Yay!</h3>
   </div>
 );
 
