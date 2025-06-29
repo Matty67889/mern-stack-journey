f1c142faac7e6fc68c9ff0e29430d5c053052155
Matthew Mentis-Cort <matty67889@gmail.com>
2025-06-29 16:11:40 -0400

    Add GraphQl About API
    - Create AboutAPI with GraphQL

--- mern-app/server/server.js
+++ mern-app/server/server.js
@@ -1,5 +1,31 @@
+const fs = require('fs');
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+
+// GraphQL Nonsense
+let aboutMessage = "Issue Tracker API v1.0";
+
+const resolvers = {
+  Query: {
+    about: () => aboutMessage,
+  },
+  Mutation: {
+    setAboutMessage
+  },
+}
+
+function setAboutMessage(_, { message }) {
+  return aboutMessage = message;
+}
+
 const app = express();
+
+// Apollo server (testing) setup
+const server = new ApolloServer({
+  typeDefs: fs.readFileSync('./server/schema.graphql', 'utf-8'),
+  resolvers
+});
+server.applyMiddleware({ app, path: '/graphql' });
 
 const fileServerMiddleware = express.static('public');
 app.use('/', fileServerMiddleware);
