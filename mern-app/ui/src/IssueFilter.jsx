3150ac06d4ae0f11abf8b797158bb57e591fd2fe
Matthew Mentis-Cort <matty67889@gmail.com>
2025-12-29 12:00:56 -0500

    Switch filter to dropdown
    - Switch filter to dropdown for
    programmatic navigation

--- mern-app/ui/src/IssueFilter.jsx
+++ mern-app/ui/src/IssueFilter.jsx
@@ -6,27 +6,47 @@
 /* eslint "react/prefer-stateless-function": "off" */
 
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 
 /**
  * Returns a component representing a filter for issues.
  * 
  * @returns an element representing a filter for issues.
  */
-export default class IssueFilter extends React.Component {
+class IssueFilter extends React.Component {
+  constructor() {
+    super();
+    this.onChangeStatus = this.onChangeStatus.bind(this);
+  }
+
+  onChangeStatus(e) {
+    const status = e.target.value;
+    // this prop is available due to `withRouter` function
+    const { history } = this.props;
+    // changing link will trigger reload of data
+    history.push({
+      pathname: 'issues',
+      search: status ? `status=${status}` : '',
+    });
+  }
+
   render() {
     return (
       <div>
-        <Link to="/issues">All Issues</Link>
-        {' | '}
-        <Link to={{ pathname: '/issues', search: '?status=New' }}>
-          New Issues
-        </Link>
-        {' | '}
-        <Link to={{ pathname: '/issues', search: '?status=Assigned' }}>
-          Assigned Issues
-        </Link>
+        Status:
+        {' '}
+        <select onChange={this.onChangeStatus}>
+          <option value="">(All)</option>
+          <option value="New">New</option>
+          <option value="Assigned">Assigned</option>
+          <option value="Fixed">Fixed</option>
+          <option value="Closed">Closed</option>
+        </select>
       </div>
     );
   }
 }
+
+// `withRouter` allows IssueFilter to have access to the router
+// object and properties
+export default withRouter(IssueFilter);
