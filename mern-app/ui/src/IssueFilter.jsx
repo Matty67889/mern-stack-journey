a9e64dfbe155d8ae259c56a0da9d03bc3d1b90eb
Matthew Mentis-Cort <matty67889@gmail.com>
2026-01-21 12:21:02 -0500

    Convert filter forms to bootstrap
    - Convert effort and status filters
    to React Bootstrap

--- mern-app/ui/src/IssueFilter.jsx
+++ mern-app/ui/src/IssueFilter.jsx
@@ -7,7 +7,9 @@
 
 import React from 'react';
 import { withRouter } from 'react-router-dom';
-import { Button } from 'react-bootstrap';
+import { ButtonToolbar, Button, FormGroup, FormControl,
+  ControlLabel, InputGroup,
+} from 'react-bootstrap';
 
 /**
  * Returns a component representing a filter for issues.
@@ -117,41 +119,41 @@ class IssueFilter extends React.Component {
     const { effortMin, effortMax } = this.state;
     return (
       <div>
-        Status:
+        <FormGroup>
+          <ControlLabel>Status:</ControlLabel>
+          <FormControl
+            componentClass="select"
+            value={status}
+            onChange={this.onChangeStatus}
+          >
+            <option value="">(All)</option>
+            <option value="New">New</option>
+            <option value="Assigned">Assigned</option>
+            <option value="Fixed">Fixed</option>
+            <option value="Closed">Closed</option>
+          </FormControl>
+        </FormGroup>
+        <FormGroup>
+          <ControlLabel>Effort between:</ControlLabel>
+          <InputGroup>
+            <FormControl value={effortMin} onChange={this.onChangeEffortMin} />
+            <InputGroup.Addon>-</InputGroup.Addon>
+            <FormControl value={effortMax} onChange={this.onChangeEffortMax} />
+          </InputGroup>
+        </FormGroup>
         {' '}
-        <select value={status} onChange={this.onChangeStatus}>
-          <option value="">(All)</option>
-          <option value="New">New</option>
-          <option value="Assigned">Assigned</option>
-          <option value="Fixed">Fixed</option>
-          <option value="Closed">Closed</option>
-        </select>
-        {' '}
-        Effort between:
-        {' '}
-        <input
-          size={5}
-          value={effortMin}
-          onChange={this.onChangeEffortMin}
-        />
-        {' - '}
-        <input
-          size={5}
-          value={effortMax}
-          onChange={this.onChangeEffortMax}
-        />
-        {' '}
-        <Button bsStyle="primary" type="button" onClick={this.applyFilter}>
-          Apply
-        </Button>
-        {' '}
-        <Button
-          type="button"
-          onClick={this.showOriginalFilter}
-          disabled={!changed}
-        >
-          Reset
-        </Button>
+        <ButtonToolbar>
+          <Button bsStyle="primary" type="button" onClick={this.applyFilter}>
+            Apply
+          </Button>
+          <Button
+            type="button"
+            onClick={this.showOriginalFilter}
+            disabled={!changed}
+          >
+            Reset
+          </Button>
+        </ButtonToolbar>
       </div>
     );
   }
